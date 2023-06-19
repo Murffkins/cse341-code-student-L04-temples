6423e58ee4a8209820b06d24eff7a7f1f9214f10
Nicole Murff <mur20031@byui.edu>
2023-06-19 17:41:44 -0500

    changes to temples program

--- routes/temple.js
+++ routes/temple.js
@@ -1,9 +1,22 @@
 const routes = require('express').Router();
 const temples = require('../controllers/temple.js');
 
+// Retrieve all published Temples
 routes.get('/', temples.findAll);
+
+// Retrieve a single Temple with id
 routes.get('/:temple_id', temples.findOne);
 
+// Create a new Temple
 routes.post('/', temples.create);
+
+// Update a Temple with id
+// routes.put('/:id', temples.update);
+
+// Delete a Temple with id
+// routes.delete('/:id', temples.delete);
+
+// Create a new Temple
+// routes.delete('/', temples.deleteAll);
 
 module.exports = routes;
